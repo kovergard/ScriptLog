6c2e36aae8d25b21657286e170008cdae5f0b5d7
Kaare Overgaard <kov@nojava.dk>
2022-07-11 13:11:24 +0200

    Add more test for better code coverage

--- tests/Unit/Public/Out-ScriptLog.Tests.ps1
+++ tests/Unit/Public/Out-ScriptLog.Tests.ps1
@@ -1,21 +1,50 @@
+BeforeDiscovery {
+    $ModuleName = 'ScriptLog'
+}
+
+BeforeAll {
+    $ModuleName = 'ScriptLog'
+    Import-Module $ModuleName -Force
+}
+
 Describe 'Out-ScriptLog' {
-    # The following cannot be tested before a function has been added to purge all ScriptLogs objects.
-    <#
-        It 'Trying to send a message to a ScriptLog should fail if no ScriptLog objects has been instantiated' {
-        { 'Test' | Out-ScriptLog } | Should -Throw
-    } #>
+
+    It 'should throw an error if trying to send a message if no ScriptLog has been defined' {
+        { 'Test' | Out-ScriptLog } | Should -Throw 'Use New-ScriptLog to create a ScriptLog before using Out-ScriptLog'
+    }
+
     It 'must have a message count of 1 after adding a single message' {
         $ScriptLog = New-ScriptLog
         'Test' | Out-ScriptLog -Log $ScriptLog
         $ScriptLog.Messages.Count | Should -Be 1
     }
 
-    It 'should output different severities to the respective channels.' {
+    It 'should output messages with severity Information using Write-Information' {
+        $ScriptLog = New-ScriptLog -MessagesOnConsole Information
+        Mock -ModuleName $ModuleName -CommandName 'Write-Information' -MockWith { return 'Test.Information' } -ParameterFilter { $MessageData -eq 'Test.Information' }
+        Out-ScriptLog -Message 'Test.Information' -Log $ScriptLog -Severity Information -InformationAction Continue | Should -Be 'Test.Information'
+        Assert-MockCalled -ModuleName $ModuleName -CommandName 'Write-Information' -Exactly 1 -Scope it
+    }
+
+    It 'should output messages with severity Verbose using Write-Verbose' {
+        $ScriptLog = New-ScriptLog -MessagesOnConsole Verbose
+        Mock -ModuleName $ModuleName -CommandName 'Write-Verbose' -MockWith { return 'Test.Verbose' } -ParameterFilter { $Message -eq 'Test.Verbose' }
+        Out-ScriptLog -Message 'Test.Verbose' -Log $ScriptLog -Severity Verbose | Should -Be 'Test.Verbose'
+        Assert-MockCalled -ModuleName $ModuleName -CommandName 'Write-Verbose' -Exactly 1 -Scope it
+    }
+
+    It 'should output messages with severity Warning using Write-Warning' {
         $ScriptLog = New-ScriptLog
-        Out-ScriptLog -Message 'Information' -Log $ScriptLog -Severity Information
-        Out-ScriptLog -Message 'Verbose' -Log $ScriptLog -Severity Verbose -Verbose
-        Out-ScriptLog -Message 'Warning' -Log $ScriptLog -Severity Warning
-        Out-ScriptLog -Message 'Error' -Log $ScriptLog -Severity Error
+        Mock -ModuleName $ModuleName -CommandName 'Write-Warning' -MockWith { return 'Test.Warning' } -ParameterFilter { $Message -eq 'Test.Warning' }
+        Out-ScriptLog -Message 'Test.Warning' -Log $ScriptLog -Severity Warning | Should -Be 'Test.Warning'
+        Assert-MockCalled -ModuleName $ModuleName -CommandName 'Write-Warning' -Exactly 1 -Scope it
+    }
+
+    It 'should output messages with severity Error using Write-Error' {
+        $ScriptLog = New-ScriptLog
+        Mock -ModuleName $ModuleName -CommandName 'Write-Error' -MockWith { return 'Test.Error' } -ParameterFilter { $Message -eq 'Test.Error' }
+        Out-ScriptLog -Message 'Test.Error' -Log $ScriptLog -Severity Error | Should -Be 'Test.Error'
+        Assert-MockCalled -ModuleName $ModuleName -CommandName 'Write-Error' -Exactly 1 -Scope it
     }
 
     It 'should truncate long messages in CMTrace format.' {
